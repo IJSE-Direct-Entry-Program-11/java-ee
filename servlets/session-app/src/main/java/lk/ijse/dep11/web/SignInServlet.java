93cc4b12f178e226c5335fddd0e90c99227151b7
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-11-08 19:32:02 +0530

    Finish the app

--- servlets/session-app/src/main/java/lk/ijse/dep11/web/SignInServlet.java
+++ servlets/session-app/src/main/java/lk/ijse/dep11/web/SignInServlet.java
@@ -5,15 +5,13 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 @WebServlet("/sign-in")
 public class SignInServlet extends HttpServlet {
@@ -23,6 +21,7 @@ public class SignInServlet extends HttpServlet {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         String mode = req.getParameter("mode");
+        String rememberMe = req.getParameter("remember-me");
 
         BasicDataSource pool = (BasicDataSource) getServletContext().getAttribute("connectionPool");
         try (Connection connection = pool.getConnection()) {
@@ -33,6 +32,20 @@ public class SignInServlet extends HttpServlet {
             if (rst.next()){
                 if (rst.getString("password").equals(DigestUtils.sha256Hex(password))){
                     HttpSession session = req.getSession();
+
+                    if (rememberMe != null){
+                        String setCookieHeader = resp.getHeader("Set-Cookie");
+                        setCookieHeader += "; Max-Age=" + (60 * 30);
+                        resp.setHeader("Set-Cookie", setCookieHeader);
+                    }
+
+                    Cookie ijseCookie = new Cookie("ijse", "dep-11");
+                    ijseCookie.setMaxAge(60 * 30);
+                    ijseCookie.setHttpOnly(true);
+                    // ijseCookie.setSecure(true);
+                    resp.addCookie(ijseCookie);
+
+                    // session.setMaxInactiveInterval(60 * 2);
                     session.setAttribute("fullName", rst.getString("full_name"));
                     session.setAttribute("mode", mode == null ? "dark" : mode);
                     resp.sendRedirect(req.getContextPath());
